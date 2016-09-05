99a4054edd84f52b6f9dbcc59b7c20c6495a6bb1
Camille Garcia <camgarcia53@gmail.com>
2016-09-05 12:17:10 -0400

    Wireframe
    Wireframe coding using HTML5, CSS3, Bootstrap, JavaScript, jQuery

---
+++ widget.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var url="../data/employees.json"
+	$.getJSON(url, function (response));
+		var statusHTML = '<ul class="bulleted">';
+	});
+
+});
+
+
+
+
+$.each(aarray, function(index, value)
+
+
+
+function workLoad {
+
+$.ajaxSetup ( cache: false));
+}
